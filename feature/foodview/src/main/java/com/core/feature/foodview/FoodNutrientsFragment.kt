fc813676c20be684152b3d4eeadb04a0c17ef05e
Mateus <mateusfreesedossantos@gmail.com>
2024-11-15 04:21:09 -0300

    feature(new-ui): implement new food detail screen ui
    Signed-off-by: Mateus <mateusfreesedossantos@gmail.com>

--- feature/foodview/src/main/java/com/core/feature/foodview/FoodNutrientsFragment.kt
+++ feature/foodview/src/main/java/com/core/feature/foodview/FoodNutrientsFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.mfs.core.model.Food
 import com.mfs.feature.foodview.R
 import com.mfs.feature.foodview.databinding.FragmentFoodNutrientsBinding
+import com.mfs.feature.foodview.databinding.NutrientItemBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -42,11 +44,44 @@ class FoodNutrientsFragment : Fragment() {
     }
 
     private fun handleFood(food: Food) = binding.apply {
-        tvName.text = getString(R.string.name, food.name)
-        tvCalories.text = getString(R.string.calories, food.calories)
-        tvCarbohydrate.text = getString(R.string.carbohydrate, food.carbohydrate)
-        tvProtein.text = getString(R.string.protein, food.protein)
-        tvFat.text = getString(R.string.fat, food.fat)
+        val nutrients = listOf(
+            Triple(R.string.calories, food.calories, R.string.amount_kcal),
+            Triple(R.string.protein, food.protein, R.string.amount_grams),
+            Triple(R.string.carbohydrate, food.carbohydrate, R.string.amount_grams),
+            Triple(R.string.fat, food.fat, R.string.amount_grams)
+        )
+
+        tvName.text = food.name
+        lvMacronutrients.adapter = object : BaseAdapter() {
+            override fun getCount(): Int = nutrients.size
+
+            override fun getItem(position: Int): Any {
+                return nutrients[position]
+            }
+
+            override fun getItemId(position: Int): Long {
+                return nutrients[position].first.toLong()
+            }
+
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+                return convertView ?: NutrientItemBinding.inflate(
+                    layoutInflater,
+                    parent,
+                    false
+                ).bindNutrientItem(
+                    nutrients[position]
+                ).root
+            }
+
+            fun NutrientItemBinding.bindNutrientItem(
+                nutrient: Triple<Int, Double?, Int>
+            ): NutrientItemBinding {
+                return this@bindNutrientItem.apply {
+                    tvNutrientName.setText(nutrient.first)
+                    tvNutrientValue.text = getString(nutrient.third, nutrient.second)
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
