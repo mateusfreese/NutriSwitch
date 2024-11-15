bc34bbea6a75c1413a2173f90bdcab3f1e63c9e9
Mateus <mateusfreesedossantos@gmail.com>
2024-11-14 23:20:32 -0300

    project-configuration: bump compileSdk and targetSdk from api 34 to 35
    Signed-off-by: Mateus <mateusfreesedossantos@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.mfs.nutriswitch"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mfs.nutriswitch"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
